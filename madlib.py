9c03630d5a04498abed497d9005898849624bcc5
Daniel Quinn <dandidit.quinn@gmail.com>
2023-06-24 09:48:03 -0700

    save4later

--- madlib.py
+++ madlib.py
@@ -6,68 +6,68 @@ def welcome_message():
     welcome_message = "Its Madlib time! Get ready to make some fun Madlibs! Fill in the blanks with funny words and see what comes out!"
 
     print(welcome_message)
-welcome_message()
+
 
 # open the file into string
 def read_template(file_path):
+  non_curly = False
+  story_dictionary = ''
+  story_list = []
+  word_list = []
+  empty_filler = ''
+  ignore = ["{","}"]
+  new_list = []
   with open('madlib.txt', "r")as file:
     template = file.read()
   
 
-non_curly = False
-story_dictionary = ''
-story_list = []
-word_list = []
-empty_filler = ''
-ignore = ["{","}"]
-new_list = []
 
-# parse template into useable parts
-for char in template:
-    if char == "}":
-        word_list.append(empty_filler)
-        empty_filler = ''
-        non_curly = False
+      # parse template into useable parts
+    for char in template:
+      if char == "}":
+          word_list.append(empty_filler)
+          empty_filler = ''
+          non_curly = False
 
-    if char == "{":
-        # when changing value use single =
-        non_curly = True
-        story_dictionary = ''
+      if char == "{":
+          # when changing value use single =
+          non_curly = True
+          story_dictionary = ''
 
-    if non_curly == False:
-        if char not in ignore:
-          story_dictionary += char
+      if non_curly == False:
+          if char not in ignore:
+            story_dictionary += char
 
-    else:
-        if char not in ignore:
-          empty_filler += char
+      else:
+          if char not in ignore:
+            empty_filler += char
 
 
-# print story list
-    
-# print(word_list)
+          # print story list
+              
+          # print(word_list)
 
-# prompt user to submit words for matching spaces
-for i in word_list:  
-  user_input = input(f"enter a(n) {i}: ")
-#  take user input and populate the template with user answer
-  updated_string = new_list.append(user_input)
+  for i in word_list:  
+    user_input = input(f"enter a(n) {i}: ")
+      #  take user input and populate the template with user answer
+    updated_string = new_list.append(user_input)
 
-print(new_list)
-#  give user back completed response
+  print(new_list)
+        #  give user back completed response
+        # prompt user to submit words for matching spaces
+  return template
 
-def parse_template(template:)
+def parse_template(template):
   pattern = r"\{([^}]*)\}"
   result = re.sub(pattern, lambda word: new_list.pop(0), template)
   print(result)
 
-def merge(stripped_template)
+def merge(stripped_template):
 
 
 
-with open('finished_madlib.txt', "w")as file:
+  with open('finished_madlib.txt', "w")as file:
     template = file.write(result)
 
-if __name__ = "__main__":
-  pass
-  
+if __name__ == "__main__":
+  welcome_message()
